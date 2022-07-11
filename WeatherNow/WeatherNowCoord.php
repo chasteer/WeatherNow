6301f43cd91221eca84bce626020e95fd9251fa7
chasteer <44359860+chasteer@users.noreply.github.com>
2022-07-11 08:27:20 +0500

    Update WeatherNowCoord.php

--- WeatherNow/WeatherNowCoord.php
+++ WeatherNow/WeatherNowCoord.php
@@ -13,17 +13,7 @@ class WeatherNowCoord
             curl_close($ch);            
             $res = json_decode($res);
             return array($res->lat,$res->lon);
-        }
-        else{
-            $ch = curl_init('http://ip-api.com/json/' .'178.72.91.59' . '?lang=ru');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_HEADER, false);
-            $res = curl_exec($ch);
-            curl_close($ch);            
-            $res = json_decode($res);
-            return array($res->lat,$res->lon);
-        }
+        }        
         return array('55.7522200','37.6155600');//По умолчанию Москва
     }
 }
