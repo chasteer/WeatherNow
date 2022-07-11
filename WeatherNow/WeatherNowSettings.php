6256376fa8949f1066356217e9d4bab87c8e7b11
chasteer <44359860+chasteer@users.noreply.github.com>
2022-07-11 08:26:59 +0500

    Update WeatherNowSettings.php

--- WeatherNow/WeatherNowSettings.php
+++ WeatherNow/WeatherNowSettings.php
@@ -1,15 +1,15 @@
 <? 
 class WeatherNowSettings{
     public static function GetAPIKeyYandex(){
-        $YandexAPIKey = 'b8c75046-2d66-4174-97ed-1f6ba10dda7a';
+        $YandexAPIKey = '<Yandex_API>';
         return $YandexAPIKey;
     }
     public static function GetAPIKeyGismeteo(){
-        $GismeteoAPIKey = '56b30cb255.3443075';
+        $GismeteoAPIKey = 'GismeteoAPIK';
         return $GismeteoAPIKey;
     }
     public static function GetAPIKeyOpenWeatherMap(){
-        $GismeteoAPIKey = 'c4e6f751a6bc8c3f2013f583e7ff80f6';
+        $GismeteoAPIKey = 'GismeteoAPI';
         return $GismeteoAPIKey;
     }
 }
